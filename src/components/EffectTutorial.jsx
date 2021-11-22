e6bb250fc612671d1b5e55a9a39a922b6b6c0cb9
Irene <irenesis123@gmail.com>
2021-11-21 21:55:12 -0500

    useEffect section donezo

--- src/components/EffectTutorial.jsx
+++ src/components/EffectTutorial.jsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const EffectTutorial = () => {
-  return <div></div>;
+  const [data, setData] = useState('');
+  // useEffect will be called on rerender (when state changes) and when instantiated. i.e. componentDidMount and setState
+  // primarily used for API calls when loading a page
+  useEffect(() => {
+    console.log('yo the component is instantiated');
+    axios
+      .get('https://jsonplaceholder.typicode.com/comments')
+      .then((response) => {
+        // console.log(response.data);
+        console.log('api is called');
+      });
+    // pass array of all states I want to listen to. here I listen to nothing so useEffect only runs on mount
+  }, []);
+
+  return (
+    <>
+      <h2>useEffect</h2>
+      <section>Hello World</section>
+    </>
+  );
 };
 
 export default EffectTutorial;
